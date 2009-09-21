d03fe6e7c4487e6cf9ce77491f8cd80fa118d950
gauvain <gauvain@d7701b40-b1f3-49c6-8f30-ab10d5252e56>
2009-09-21 04:56:25 +0000

    backup what we have on firex
    git-svn-id: https://svn.ulteo.com/tools@15 d7701b40-b1f3-49c6-8f30-ab10d5252e56

--- build-scripts/ovd-deb.py
+++ build-scripts/ovd-deb.py
@@ -18,136 +18,294 @@
 
 import os, sys, re
 import getopt
+import shutil
 import time
+from subprocess import Popen, STDOUT, PIPE
+import glob
+import atexit
 
 BASE_DIR = '/home/gauvain/ovddebs'
 LOCK_FILE = '%s/.locked'%BASE_DIR
+LOGS_DIR = '%s/logs'%BASE_DIR
 BUILD_DIR = '%s/build'%BASE_DIR
 RESULTS_DIR = '%s/results'%BASE_DIR
-SVN_BASE_DIR = '%s/ovd'%BASE_DIR
+SVN_BASE_DIR = '%s/svn'%BASE_DIR
 DEFAULT_BRANCH = 'trunk'
 
+AUTOTOOLS_CMD = [['./autogen.sh'], ['./configure'], ['make', 'distcheck']]
+ANT_CMD = [['./bootstrap'], ['ant', 'dist']]
 
-packages = { 'SessionManager': 'ovd-session-manager',
-             'ApplicationServer': 'ovd-application-server',
-             'chroot-apps': 'ovd-chroot-apps',
-             'meta': 'ulteo-ovd' }
+DEBUG = False
+
+packages = { 'SessionManager': ['ovd-session-manager', AUTOTOOLS_CMD],
+             'ApplicationServer': ['ovd-application-server', AUTOTOOLS_CMD],
+             'chroot-apps': ['ovd-chroot-apps', AUTOTOOLS_CMD],
+             'client/java': ['ovd-applets', ANT_CMD],
+             'meta': ['ulteo-ovd', ''] }
 # [ svn location, version base, repository ]
-branches = { '1.1': ['branches/1.1', '1.1~~+svn', '1.1-staging'],
-             '2.0': ['trunk', '2.0~~+svn', '2.0-staging'],
-             'trunk': ['trunk', '2.0~~+svn', '2.0-staging'] }
+branches = { '1.1': ['branches/1.1', None, '1.1-staging'],
+             '2.0': ['trunk', None, '2.0-staging'],
+             'trunk': ['trunk', None, '2.0-staging'] }
 
 sumup = {}
 
-def svn_up ():
-    os.chdir(SVN_BASE_DIR)
-    os.system("svn up")
+def log_start(message):
+    print message + '... ',
+    sys.stdout.flush()
 
-def get_revno ():
-    os.chdir("%s/trunk"%SVN_BASE_DIR)
+def log_end(success):
+    if success:
+        print 'OK'
+    else:
+        print 'FAILED'
+
+def init():
+    svn_up()
+    for branch in branches.keys():
+        branches[branch][1] = get_base_version (branches[branch][0])
+
+def svn_up():
+    log_start("Updating the svn tree")
+    log_end (run(['svn', 'up'], cwd=SVN_BASE_DIR))
+
+def get_revno():
+    os.chdir('%s/trunk'%SVN_BASE_DIR)
     cmd = """svn info SessionManager | awk '/^Revision: / {printf "%05d", $2}'"""
     revno = os.popen(cmd).readline()
     return revno
 
-def make_dist (module_path):
-    return os.system ('cd %s && ./autogen.sh && ./configure && make dist'%module_path)
+def get_base_version(branch):
+    major = None
+    minor = None
+    build = None
+    major_re = re.compile(r'^m4_define.*_version_major.*\[([^\[]*)].*')
+    minor_re = re.compile(r'^m4_define.*_version_minor.*\[([^\[]*)].*')
+    build_re = re.compile(r'^m4_define.*_version_build.*\[([^\[]*)].*')
 
-def parse_tarball_name (tarball_name):
-    r = re.compile(r"(?P<package>.*)-(?P<version>.*).tar.gz")
+    lines = open('%s/%s/SessionManager/configure.in.in'%(SVN_BASE_DIR, branch)).readlines()[:3]
+
+    try:
+        major = major_re.search(lines[0]).group(1)
+        minor = minor_re.search(lines[1]).group(1)
+        try:
+            # the build field might be empty
+            build = build_re.search(lines[2]).group(1).replace('@REVISION@', '')
+        except:
+            build = ''
+        return '%s.%s%s'%(major, minor, build)
+    except:
+        return None
+
+def parse_tarball_name(tarball_name):
+    r = re.compile(r'(?P<package>.*)-(?P<version>.*).tar.gz')
     m = re.search(r,tarball_name)
     return m.group('package'), m.group('version')
 
-class DebBuild:
-    def __init__(self, module, branch='trunk', publish=True):
-        # set all the variables we'll need
-        self.revno = get_revno()
-        self.module = module
-        self.publish = publish
-        self.svn_base = "%s/%s"%(SVN_BASE_DIR,branches[branch][0])
-        self.module_path = "%s/%s"%(self.svn_base,module)
-        self.debian_path = "%s/debian/%s/debian"%(self.svn_base,packages[module])
-        self.schroot_target = branches[branch][2]
-        self.distro_target = self.schroot_target.replace(".", "-")
+def cleanup():
+    os.unlink(LOCK_FILE)
 
-        if self.module != 'meta':
-            self.tarball_name = "%s-%s%s.tar.gz"%(packages[module],branches[branch][1],self.revno)
-            self.tarball_path = "%s/%s"%(self.module_path,self.tarball_name)
-            self.package, self.version = parse_tarball_name(self.tarball_name)
-            self.deb_version = "%s-0ulteo0"%self.version
-            self.deb_orig_name = "%s_%s.orig.tar.gz"%(self.package,self.version)
-            self.deb_orig_path = "%s/%s"%(BUILD_DIR,self.deb_orig_name)
-            self.deb_src_dir = "%s/%s-%s"%(BUILD_DIR,self.package,self.version)
-            self.deb_src_debian_dir = "%s/debian"%self.deb_src_dir
-            self.deb_dsc_changes_name = "%s_%s.dsc"%(self.package,self.deb_version)
-            # FIXME: guess arch
-            self.deb_binary_changes_name = "%s_%s_i386.changes"%(self.package,self.deb_version)
+def run(args, logfile=None, cwd=None):
+    if DEBUG:
+        if cwd:
+            print "Cwd: " + cwd
+        print "Running cmd "+" ".join(args)
+
+    if logfile == -1:
+        out = sys.stdout
+    else:
+        if logfile:
+            out = open(logfile, 'a')
         else:
-            self.package = 'ulteo-ovd'
-            self.version = '%s%s'%(branches[branch][1],self.revno)
-            self.deb_version = self.version
-            self.deb_dsc_changes_name = "%s_%s.dsc"%(self.package,self.deb_version)
-            self.deb_binary_changes_name = "%s_%s_i386.changes"%(self.package,self.deb_version)
-            self.deb_src_dir  = "%s/%s-%s"%(BUILD_DIR,self.package,self.version)
+            out = open('/dev/null', 'a')
 
+    process = Popen(args, stdout=out, stderr=STDOUT, cwd=cwd)
+    process.communicate()
+
+    if logfile != -1:
+        out.close()
+
+    if not process.returncode:
+        return True
+    return False
+
+class DebBuild:
+    def __init__(self, module, branch='trunk', release=False, publish=True, on_stdout=False):
+        # set all the variables we'll need
+        self._revno = get_revno()
+        self._module = module
+        self._do_release = release
+        self._do_publish = publish
+        self._on_stdout = on_stdout
+        self._svn_base = '%s/%s'%(SVN_BASE_DIR,branches[branch][0])
+        self._module_path = '%s/%s'%(self._svn_base,module)
+        self._debian_path = '%s/debian/%s/debian'%(self._svn_base,packages[self._module][0])
+        self._schroot_target = branches[branch][2]
+        self._distro_target = self._schroot_target.replace('.', '-')
+
+        if self._module != 'meta':
+            self._tarball_name = '%s-%s%s.tar.gz'%(packages[self._module][0],branches[branch][1],self._revno)
+            self._tarball_path = '%s/%s'%(self._module_path,self._tarball_name)
+            self._package, self._version = parse_tarball_name(self._tarball_name)
+            if not self._do_release:
+                self._deb_version = '%s-0ulteo0'%self._version
+            else:
+                self._deb_version = self._get_changelog_version()
+            self._deb_orig_name = '%s_%s.orig.tar.gz'%(self._package,self._version)
+            self._deb_orig_path = '%s/%s'%(BUILD_DIR,self._deb_orig_name)
+            self._deb_src_dir = '%s/%s-%s'%(BUILD_DIR,self._package,self._version)
+            self._deb_src_debian_dir = '%s/debian'%self._deb_src_dir
+            self._deb_dsc_changes_name = '%s_%s.dsc'%(self._package,self._deb_version)
+            # TODO: guess arch
+            self._deb_binary_changes_name = '%s_%s_i386.changes'%(self._package,self._deb_version)
+            self._deb_binary_changes_file = '%s/%s'%(BUILD_DIR, self._deb_binary_changes_name)
+        else:
+            self._package = 'ulteo-ovd'
+            if not self._do_release:
+                self._version = '%s%s'%(branches[branch][1],self._revno)
+                self._deb_version = self._version
+            else:
+                self._version = branches[branch][1]
+                self._deb_version = self._get_changelog_version()
+            self._deb_dsc_changes_name = '%s_%s.dsc'%(self._package,self._deb_version)
+            self._deb_binary_changes_name = '%s_%s_i386.changes'%(self._package,self._deb_version)
+            self._deb_binary_changes_file = '%s/%s'%(BUILD_DIR, self._deb_binary_changes_name)
+            self._deb_src_dir  = '%s/%s-%s'%(BUILD_DIR,self._package,self._version)
+
+        self._logfile_name = '%s-%s_%s.txt' % (self._package, self._deb_version, int(time.time()))
+        self._logfile_dir = '%s/%s' % (LOGS_DIR, self._schroot_target)
+        if not os.path.isdir(self._logfile_dir):
+            os.makedirs(self._logfile_dir)
+        self._logfile = '%s/%s'%(self._logfile_dir, self._logfile_name)
+
+        self._sumup = { 'srcbuild': True,
+                        'debbuild': True,
+                        'publish': True }
+
+
+    def _log(self, message):
+        f = open(self._logfile, 'a')
+        f.write(message)
+        f.close()
+
+    def _run(self, args, log=True, cwd=None):
+        if log:
+            if not self._on_stdout:
+                return run(args, self._logfile, cwd)
+            else:
+                return run(args, -1, cwd)
+        else:
+            return run(args, cwd=cwd)
+
+    def _get_changelog_version(self):
+        changelog = '%s/changelog' % self._debian_path
+        f = open(changelog)
+        line = f.readline()
+        f.close()
+        r = re.compile(r'(?P<name>.*) \((?P<version>.*)\) (?P<target>.*); urgency=(?P<urgency>.*)')
+        try:
+            return r.search(line).group('version')
+        except:
+            return None
+
+    def _make_dist(self):
+        ret = True
+        for cmd in packages[self._module][1]:
+            ret = self._run (cmd, log=True, cwd=self._module_path)
+            if not ret:
+                break
+        return ret
 
     def _build_source(self):
-        os.chdir(self.svn_base)
-        if self.module != 'meta':
-            ret = make_dist (self.module_path)
-            if ret:
-                raise "make_dist() failed (%s)" % self.module
-            os.rename(self.tarball_path, self.deb_orig_path)
-            os.system('tar zxf %s -C %s'%(self.deb_orig_path,BUILD_DIR))
-            os.system('cp -R %s %s'%(self.debian_path,self.deb_src_dir))
+        log_start(" Building the source tarball")
+        if self._module != 'meta':
+            if not self._make_dist():
+                log_end(False)
+                return False
+            os.rename(self._tarball_path, self._deb_orig_path)
+            self._run(['tar', 'zxf', self._deb_orig_path, '-C', BUILD_DIR], False, self._svn_base)
+            shutil.copytree(self._debian_path, '%s/debian'%self._deb_src_dir)
         else:
-            os.system('cp -R %s/debian/%s %s/ulteo-ovd-%s'%(self.svn_base,self.package,BUILD_DIR,self.version))
-        os.system('rm -rf $(find %s -name .svn)'%self.deb_src_dir)
+            shutil.copytree('%s/debian/%s'%(self._svn_base,self._package),
+                             '%s/ulteo-ovd-%s'%(BUILD_DIR,self._version))
+        log_end(True)
 
-        os.chdir(self.deb_src_dir)
-        cmd = 'dch --force-distribution -D %s -v %s New devel snaphot'%(self.distro_target,self.deb_version)
-        os.system(cmd)
-        os.system('debuild -S -sa -us -uc')
+        # TODO: drop svn dirs using python
+        os.system('rm -rf $(find %s -name .svn)'%self._deb_src_dir)
 
+        # generate a changelog if we're not releasing
+        if not self._do_release:
+            cmd = ['dch', '--force-distribution',
+                          '-D', self._distro_target,
+                          '-v', self._deb_version,
+                          'New devel snaphot']
+            if not self._run(cmd, False, self._deb_src_dir):
+                return False
+        log_start(" Building the source package")
+        if not self._run(['debuild', '-S', '-sa', '-us', '-uc'], True, self._deb_src_dir):
+            log_end(False)
+            return False
+        log_end(True)
 
+        return True
 
     def build_deb(self):
-        self._build_source()
-        os.chdir(BUILD_DIR)
-        cmd = 'sbuild --force-orig-source -A -s -d %s %s'%(self.schroot_target,self.deb_dsc_changes_name)
-        os.system(cmd)
-
-        if os.path.isfile(self.deb_binary_changes_name):
-            return self.deb_binary_changes_name
-        else:
+        self._log('Building %s for %s\n' % (self._deb_dsc_changes_name, self._schroot_target))
+        if not self._build_source():
+            self._sumup['srcbuild'] = False
             return False
 
-    def do_publish(self):
-        os.chdir(BUILD_DIR)
-        if self.publish:
-            os.system('dput ovd-local %s'%changes)
-            os.system('/home/gauvain/bin/update-ovd-repo.sh')
+        log_start(" Building the binary package")
+        cmd = ['sbuild', '-n', '--force-orig-source',
+                         '-A', '-s', '-d', self._schroot_target,
+                         self._deb_dsc_changes_name]
+        ret = self._run(cmd, cwd=BUILD_DIR)
+        log_end(ret)
+        if not ret:
+            self._sumup['debbuild'] = False
+
+        return ret
+
+    def publish(self):
+        log_start(" Publishing the package")
+        if self._do_publish:
+            ret = self._run(['dput', 'ovd-local', self._deb_binary_changes_file], cwd=BUILD_DIR)
+            if ret:
+                ret = self._run(['/home/gauvain/bin/update-ovd-repo.sh'], cwd=BUILD_DIR)
+
+            if not ret:
+                self._sumup['publish'] = False
+
+            log_end(ret)
+
         else:
-            target_dir = "%s/%s"%(RESULTS_DIR,self.schroot_target)
+            target_dir = '%s/%s'%(RESULTS_DIR,self._schroot_target)
             if not os.path.isdir(target_dir):
                 os.makedirs(target_dir)
             for ext in ['gz','deb','udeb','dsc']:
-                cmd = "cp -v *.%s %s"%(ext,target_dir)
-                os.system(cmd)
+                for f in glob.glob("%s/*.%s"%(BUILD_DIR, ext)):
+                    shutil.copy(f, target_dir)
+
+            log_end(True)
+
+    def get_sumup(self):
+        return (self._logfile, self._sumup)
 
 
 
-
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'nb:', ['no-publish', 'branch'])
+        opts, args = getopt.getopt(sys.argv[1:],
+                           'okrnb:',
+                          ['stdout', 'keeplog', 'release', 'no-publish', 'branch'])
     except getopt.GetoptError, err:
-        print >> sys.stderr, "Error parsing the command line"
+        print >> sys.stderr, 'Error parsing the command line'
         sys.exit(1)
 
     # defaults
+    release = False
     publish = True
+    keeplog = False
+    on_stdout = False
     branch = DEFAULT_BRANCH
 
     for o, a in opts:
@@ -157,8 +315,14 @@ if __name__ == "__main__":
             if branches.has_key(a):
                 branch = a
             else:
-                print "Unknown branch: %s"%a
+                print 'Unknown branch: %s'%a
                 sys.exit(1)
+        if o in ('-r', '--release'):
+            release = True
+        if o in ('-k', '--keeplog'):
+            keeplog = True
+        if o in ('-o', '--stdout'):
+            on_stdout = True
 
     if len(args) < 1:
         to_build = packages.keys()
@@ -168,41 +332,56 @@ if __name__ == "__main__":
             if packages.has_key(k):
                 to_build.append(k)
             else:
-                print "Unknown module: %s\n"%k
+                print 'Unknown module: %s\n'%k
+
+    # check if the required modules are available, drop them if not
+    if branch in ['2.0', 'trunk']:
+        try:
+            to_build.remove('meta')
+        except:
+            pass
+    if branch in ['1.1']:
+        try:
+            to_build.remove('client/java')
+        except:
+            pass
 
     if len(to_build) == 0:
-        print "Nothing to build."
+        print 'Nothing to build.'
         sys.exit(0)
+
+    atexit.register(cleanup)
 
     while os.path.isfile(LOCK_FILE):
         print 'Build system locked; waiting...'
         time.sleep(5)
     open(LOCK_FILE, 'w').close()
 
-    svn_up()
+    init()
 
     for module in to_build:
-        os.system("rm -rf %s/*"%BUILD_DIR)
+        shutil.rmtree(BUILD_DIR, True)
+        os.makedirs(BUILD_DIR)
 
-        print "Now building module %s\n"%module
-        deb = DebBuild(module,branch,publish)
-        changes = deb.build_deb()
-        if not changes:
-            print "ERROR: module %s not built"%module
-            sumup[module] = False
-        else:
-            deb.do_publish()
-            sumup[module] = True
+        print '\nBuild started for module %s (%s)' % (module, branch)
+        deb = DebBuild(module,branch,release,publish,on_stdout)
+        if deb.build_deb():
+            deb.publish()
 
+        sumup[module] = deb.get_sumup()
 
-    txt = "\nSumup:\n"
+    text = '\n'
     for module in sumup.keys():
-        txt += "  %s => "%module
-        if sumup[module]:
-            txt += "built\n"
+        failure = False
+        for step in sumup[module][1].keys():
+            if not sumup[module][1][step]:
+                failure = step
+        text += '%s => ' % module
+        if failure:
+            text += "FAILURE (%s), log in %s\n" % (step, sumup[module][0])
         else:
-            txt += "FAILED\n"
+            text += "BUILT\n"
+            if not keeplog:
+                os.unlink(sumup[module][0])
 
-    print txt
-
-    os.unlink(LOCK_FILE)
+    print text
